ae5d6a75aca66de7c5d80f97661a67fb9f38bc30
Scott Zeta <wc070211@gmail.com>
2023-10-04 16:45:46 +1030

    query to api

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -3,8 +3,27 @@ import React from 'react';
 import { useDropzone } from 'react-dropzone';
 import { Inbox } from 'lucide-react';
 import { uploadToS3 } from '@/lib/s3';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 const FileUpload = () => {
+  //hit the api to create chat when finish upload
+  const { mutate } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post('/api/create-chat', {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
+
   const { getRootProps, getInputProps } = useDropzone({
     accept: { 'application/pdf': ['.pdf'], 'text/plain': ['.txt'] },
     maxFiles: 1,
@@ -19,7 +38,18 @@ const FileUpload = () => {
       //call function upload to s3
       try {
         const data = await uploadToS3(file);
-        console.log('data:', data);
+        if (!data?.file_key || !data?.file_name) {
+          alert('Error uploading file');
+          return;
+        }
+        mutate(data, {
+          onSuccess: (data) => {
+            console.log(data);
+          },
+          onError: (error) => {
+            console.log(error);
+          },
+        });
       } catch (error) {
         console.log(error);
       }
