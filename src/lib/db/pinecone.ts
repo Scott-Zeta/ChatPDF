fcebf6595d685be9c3798edade8cf308f1c268be
Scott Zeta <wc070211@gmail.com>
2023-10-08 20:08:43 +1030

    parsing each pages

--- src/lib/db/pinecone.ts
+++ src/lib/db/pinecone.ts
@@ -1,6 +1,8 @@
 import { PineconeClient } from "@pinecone-database/pinecone";
 import { downloadFromS3 } from "./s3-server";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { RecursiveCharacterTextSplitter, Document } from "@pinecone-database/doc-splitter";
+import { parse } from "path";
 
 let pinecone: PineconeClient | null = null;
 
@@ -21,7 +23,7 @@ export const getPineconeClietn = async () => {
 type PDFPage = {
     pageContent: string;
     metadata:{
-        loc:{pageNumbernumber:number};
+        loc:{pageNumber:number};
     }
 }
 
@@ -31,6 +33,31 @@ export async function processingForPinecone(file_key:string){
     if(!file_path) throw new Error("file not found in S3")
     const loader = new PDFLoader(file_path)
     const pages = (await loader.load()) as PDFPage[]
+    //2. parse the pdf string
+    const paragraphs = await Promise.all(pages.map(parseDocumnet))
+    return paragraphs
+}
 
-    return pages
+export const truncateStringByBytes = (str:string, bytes:number) => {
+    const enc = new TextEncoder()
+    return new TextDecoder('utf-8').decode(enc.encode(str).slice(0, bytes))
+}
+
+//parse the pdf string
+async function parseDocumnet(page: PDFPage){
+    let {pageContent, metadata} = page
+    pageContent = pageContent.replace(/\n/g, "")
+    //define splitter for content
+    const splitter = new RecursiveCharacterTextSplitter()
+    const paragraph = await splitter.splitDocuments([
+        new Document({
+            pageContent,
+            metadata:{
+                ...metadata,
+                pageNumber: metadata.loc.pageNumber,
+                text: truncateStringByBytes(pageContent, 3600)
+            }
+        })
+    ])
+    return paragraph
 }
