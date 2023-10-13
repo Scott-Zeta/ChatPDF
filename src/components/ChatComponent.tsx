8c6611724ac235510cf9f8df587eb1f0a62a3145
Scott Zeta <wc070211@gmail.com>
2023-10-13 17:28:10 +1030

    input components

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,9 +1,13 @@
 'use client';
 import React from 'react';
+import { Input } from './ui/input';
+import { useChat } from 'ai/react';
 
 type Props = {};
 
 const ChatComponent = (props: Props) => {
+  //input state manager by ai/react
+  const { input, handleInputChange, handleSubmit } = useChat();
   return (
     <div
       className="relative max-h-screen overflow-scroll"
@@ -13,8 +17,10 @@ const ChatComponent = (props: Props) => {
       <div className="sticky top-0 inset-x-0 p-2 bg-white h-fit">
         <h3 className="text-xl font-bold">Chat</h3>
       </div>
-
       {/* message list */}
+      <form onSubmit={handleSubmit}>
+        <Input value={input} onChange={handleInputChange} />
+      </form>
     </div>
   );
 };
