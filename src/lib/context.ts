940b7756a86e9b47f0329a78a77eedf823a4f823
Scott Zeta <wc070211@gmail.com>
2023-10-14 20:07:05 +1030

    indentation

--- src/lib/context.ts
+++ src/lib/context.ts
@@ -1,48 +1,59 @@
-import axios from "axios";
-import { convertToAscii } from "./utils";
-import { getEmbedding } from "./embeddings";
+import axios from 'axios';
+import { convertToAscii } from './utils';
+import { getEmbedding } from './embeddings';
 
-export async function getMatchesFromEmbeddings(embeddings: number[], file_key:string) {
-    const namespace = convertToAscii(file_key)
-    const options = {
-        method: 'POST',
-        url: `${process.env.PINECONE_HOST}/query`,
-        headers: {
-        accept: 'application/json',
-        'content-type': 'application/json',
-        'Api-Key': '***REMOVED***'
-        },
-        data: {
-            includeValues: false,
-            includeMetadata: true,
-            namespace: namespace,
-            topK: 8,
-            vector: embeddings,
-        }
-    };
-    // axios
-    //     .request(options)
-    //     .then(function (response) {
-    //         console.log(response.data);
-    //         // return response.data
-    //     })
-    //     .catch(function (error) {
-    //         console.error("Get context Error: ", error);
-    //     });
-    try {
-        const res = await fetch(options.url, {
-            method: 'POST',
-            headers: options.headers,
-            body: JSON.stringify(options.data)
+export async function getMatchesFromEmbeddings(
+  embeddings: number[],
+  file_key: string
+) {
+  const namespace = convertToAscii(file_key);
+  const options = {
+    method: 'POST',
+    url: `${process.env.PINECONE_HOST}/query`,
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'Api-Key': '***REMOVED***',
+    },
+    data: {
+      includeValues: false,
+      includeMetadata: true,
+      namespace: namespace,
+      topK: 8,
+      vector: embeddings,
+    },
+  };
+  /*
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data);
+            // return response.data
         })
-        const data = await res.json()
-        console.log(data)
-    } catch (error) {
-        console.error("Get context Error: ", error);
-    }
+        .catch(function (error) {
+            console.error("Get context Error: ", error);
+        });
+    */
+
+  // I have no idea why axios is not working here, have to use fetch
+  // Mother fucker
+  try {
+    const res = await fetch(options.url, {
+      method: 'POST',
+      headers: options.headers,
+      body: JSON.stringify(options.data),
+    });
+    const data = await res.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Get context Error: ', error);
+  }
 }
 
-export async function getContext(query: string, file_key:string) {
-    const queryEmbeddings = await getEmbedding(query)
-    const matchesVectors = await getMatchesFromEmbeddings(queryEmbeddings, file_key)
+export async function getContext(query: string, file_key: string) {
+  const queryEmbeddings = await getEmbedding(query);
+  const matchesVectors = await getMatchesFromEmbeddings(
+    queryEmbeddings,
+    file_key
+  );
 }
--- src/lib/pinecone.ts
+++ src/lib/pinecone.ts
@@ -1,67 +1,70 @@
-import { Pinecone, PineconeRecord } from "@pinecone-database/pinecone";
-import { downloadFromS3 } from "./s3-server";
-import { PDFLoader } from "langchain/document_loaders/fs/pdf";
-import { RecursiveCharacterTextSplitter, Document } from "@pinecone-database/doc-splitter";
-import { getEmbedding } from "./embeddings";
-import md5 from "md5";
-import axios from "axios";
-import { convertToAscii } from "./utils";
+import { Pinecone, PineconeRecord } from '@pinecone-database/pinecone';
+import { downloadFromS3 } from './s3-server';
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf';
+import {
+  RecursiveCharacterTextSplitter,
+  Document,
+} from '@pinecone-database/doc-splitter';
+import { getEmbedding } from './embeddings';
+import md5 from 'md5';
+import axios from 'axios';
+import { convertToAscii } from './utils';
 
 //connect to pinecone db
 export const getPineconeClient = async () => {
-    return new Pinecone({
-        environment: process.env.PINECONE_ENVIRONMENT!,
-        apiKey: process.env.PINECONE_API_KEY!,
-    })
-}
+  return new Pinecone({
+    environment: process.env.PINECONE_ENVIRONMENT!,
+    apiKey: process.env.PINECONE_API_KEY!,
+  });
+};
 
 //data processing
 
 type PDFPage = {
-    pageContent: string;
-    metadata:{
-        loc:{pageNumber:number};
-    }
-}
+  pageContent: string;
+  metadata: {
+    loc: { pageNumber: number };
+  };
+};
 
-export async function processingForPinecone(file_key:string){
-    //1. download from s3 and read the pdf
-    const file_path = await downloadFromS3(file_key)
-    if(!file_path) throw new Error("file not found in S3")
-    const loader = new PDFLoader(file_path)
-    const pages = (await loader.load()) as PDFPage[]
-    
-    //2. parse the pdf string
-    //considering to convert into single page to prevent paragraph cross multiple pages.
-    const paragraphs = await Promise.all(pages.map(parseDocument))
-    
-    //3. convert paragraphs into vector pinecon accpetable by openai model
-    const vectors = await Promise.all(paragraphs.flat().map(embeddingParagraph))
+export async function processingForPinecone(file_key: string) {
+  //1. download from s3 and read the pdf
+  const file_path = await downloadFromS3(file_key);
+  if (!file_path) throw new Error('file not found in S3');
+  const loader = new PDFLoader(file_path);
+  const pages = (await loader.load()) as PDFPage[];
 
-   //4. Since the bug confirmed in Pinecone SDK, use axios do api call
-    const options = {
-        method: 'POST',
-        url: `${process.env.PINECONE_HOST}/vectors/upsert`,
-        headers: {
-        accept: 'application/json', 
-        'content-type': 'application/json',
-        'Api-Key': `${process.env.PINECONE_API_KEY}`
-        },
-        data: {
-            vectors: vectors,
-            namespace: convertToAscii(file_key)
-        }
-    };
-    axios
-        .request(options)
-        .then(function (response) {
-        console.log(response.data);
-        })
-        .catch(function (error) {
-        console.error("Upload to pinecone error: ", error);
-        });
-        
-    return vectors
+  //2. parse the pdf string
+  //considering to convert into single page to prevent paragraph cross multiple pages.
+  const paragraphs = await Promise.all(pages.map(parseDocument));
+
+  //3. convert paragraphs into vector pinecon accpetable by openai model
+  const vectors = await Promise.all(paragraphs.flat().map(embeddingParagraph));
+
+  //4. Since the bug confirmed in Pinecone SDK, use axios do api call
+  const options = {
+    method: 'POST',
+    url: `${process.env.PINECONE_HOST}/vectors/upsert`,
+    headers: {
+      accept: 'application/json',
+      'content-type': 'application/json',
+      'Api-Key': `${process.env.PINECONE_API_KEY}`,
+    },
+    data: {
+      vectors: vectors,
+      namespace: convertToAscii(file_key),
+    },
+  };
+  axios
+    .request(options)
+    .then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.error('Upload to pinecone error: ', error);
+    });
+
+  return vectors;
 }
 
 // export const truncateStringByBytes = (str:string, bytes:number) => {
@@ -70,38 +73,38 @@ export async function processingForPinecone(file_key:string){
 // }
 
 //parse the pdf string
-async function parseDocument(page: PDFPage){
-    let {pageContent, metadata} = page
-    pageContent = pageContent.replace(/\n/g, " ")
-    //define splitter for content
-    const splitter = new RecursiveCharacterTextSplitter()
-    const paragraph = await splitter.splitDocuments([
-        new Document({
-            pageContent,
-            metadata:{
-                ...metadata,
-                pageNumber: metadata.loc.pageNumber,
-                // text: truncateStringByBytes(pageContent, 3600)
-            }
-        })
-    ])
-    return paragraph
+async function parseDocument(page: PDFPage) {
+  let { pageContent, metadata } = page;
+  pageContent = pageContent.replace(/\n/g, ' ');
+  //define splitter for content
+  const splitter = new RecursiveCharacterTextSplitter();
+  const paragraph = await splitter.splitDocuments([
+    new Document({
+      pageContent,
+      metadata: {
+        ...metadata,
+        pageNumber: metadata.loc.pageNumber,
+        // text: truncateStringByBytes(pageContent, 3600)
+      },
+    }),
+  ]);
+  return paragraph;
 }
 
 //embedding the paragraph
-async function embeddingParagraph(paragraph: Document){
-    try {
-        const embeddings = await getEmbedding(paragraph.pageContent)
-        const hash = md5(paragraph.pageContent)
-        return {
-            id:hash,
-            values: embeddings,
-            metadata: {
-                text: paragraph.pageContent
-            }
-        } as PineconeRecord
-    } catch (error) {
-        console.error("embedding error: ", error)
-        throw error
-    }
+async function embeddingParagraph(paragraph: Document) {
+  try {
+    const embeddings = await getEmbedding(paragraph.pageContent);
+    const hash = md5(paragraph.pageContent);
+    return {
+      id: hash,
+      values: embeddings,
+      metadata: {
+        text: paragraph.pageContent,
+      },
+    } as PineconeRecord;
+  } catch (error) {
+    console.error('embedding error: ', error);
+    throw error;
+  }
 }
