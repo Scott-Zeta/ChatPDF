d68b42fd43eff2abda46e9acb4aaeca813cdbf5c
Scott Zeta <wc070211@gmail.com>
2023-10-17 20:36:09 +1030

    fix CSS of sidebar

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -13,7 +13,7 @@ type Props = {
 
 const ChatSideBar = ({ chats, chatId }: Props) => {
   return (
-    <div className="w-full min-h-screen max-h-screen overflow-scroll soff p-4 text-gray-200 bg-gray-900">
+    <div className="w-full h-screen overflow-scroll p-4 text-gray-200 bg-gray-900">
       <Link href="/">
         <Button className="w-full border-dashed border-white border">
           <PlusCircle className="mr-2 w-4 h-4" />
@@ -21,7 +21,7 @@ const ChatSideBar = ({ chats, chatId }: Props) => {
         </Button>
       </Link>
 
-      <div className="flex min-h-screen max-h-screen overflow-scroll pb-20 flex-col gap-2 mt-4">
+      <div className="flex pb-2 flex-col gap-2 mt-4">
         {chats.map((chat) => (
           <Link key={chat.id} href={`/chat/${chat.id}`}>
             <div
