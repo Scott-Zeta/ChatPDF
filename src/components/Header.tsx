e7e7fd90a0f70e62b63b660b3900f9d92c31f2f3
Scott Zeta <wc070211@gmail.com>
2023-10-20 19:40:24 +1030

    update log implemented

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,20 +14,3 @@ const Header = (props: Props) => {
 };
 
 export default Header;
-
-import React from 'react';
-import UpdateLog from './UpdateLog';
-
-type Props = {};
-
-const Header = (props: Props) => {
-  return (
-    <div className="p-2 flex">
-      <div className="ml-auto">
-        <UpdateLog />
-      </div>
-    </div>
-  );
-};
-
-export default Header;
