7ce7451933985359c8dd6c4c7d46cf89ca687091
Scott Zeta <wc070211@gmail.com>
2023-10-20 19:40:24 +1030

    update log implemented

--- src/data/logs/updateEntries.ts
+++ src/data/logs/updateEntries.ts
@@ -20,3 +20,21 @@ const updateLogEntries: LogEntry[] = [
 ];
 
 export default updateLogEntries;
+type LogEntry = {
+  date: string;
+  updates: string[];
+};
+
+const updateLogEntries: LogEntry[] = [
+  {
+    date: '18/10/2023',
+    updates: ['Core function deployed'],
+  },
+  {
+    date: '20/10/2023',
+    updates: ['Implemented update log'],
+  },
+  // ...more entries
+];
+
+export default updateLogEntries;
