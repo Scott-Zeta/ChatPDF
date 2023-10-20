103581d5c6ca492fdaeb526a48966376f2f6b848
Scott Zeta <wc070211@gmail.com>
2023-10-20 19:02:17 +1030

    header with updat log

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,21 @@
 import React from 'react';
+import { Button } from './ui/button';
+
+type Props = {};
+
+const Header = (props: Props) => {
+  return (
+    <div className="p-2 flex">
+      <Button variant="link" className="ml-auto">
+        Update Log
+      </Button>
+    </div>
+  );
+};
+
+export default Header;
+
+import React from 'react';
 import UpdateLog from './UpdateLog';
 
 type Props = {};
