bdfb3870b089b31fc6d6960b319d39df5fe1aa67
Scott Zeta <wc070211@gmail.com>
2023-10-20 19:02:17 +1030

    header with updat log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { UserButton, auth } from '@clerk/nextjs';
 import { Button } from '@/components/ui/button';
 import Link from 'next/link';
 import FileUpload from '@/components/FileUpload';
+import Header from '@/components/Header';
 
 export default async function Home() {
   const { userId }: { userId: string | null } = await auth();
   const isauthenticated = !!userId;
   return (
     <div className="w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100">
+      <Header />
       <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
