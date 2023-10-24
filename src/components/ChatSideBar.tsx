9fd9f6dd4caba7319e81642b4ab9cf1a3fd9663f
Scott Zeta <wc070211@gmail.com>
2023-10-24 17:13:33 +1030

    upload in chat page, no need to back home

--- src/components/ChatSideBar.tsx
+++ src/components/ChatSideBar.tsx
@@ -6,6 +6,16 @@ import Link from 'next/link';
 import React from 'react';
 import { cn } from '@/lib/utils';
 import GoPro from './GoPro';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from '@/components/ui/dialog';
+import FileUpload from './FileUpload';
 
 type Props = {
   chats: DrizzleChat[];
@@ -15,12 +25,22 @@ type Props = {
 const ChatSideBar = ({ chats, chatId }: Props) => {
   return (
     <div className="w-full h-screen p-4 text-gray-200 bg-gray-900 flex flex-col">
-      <Link href="/">
-        <Button className="w-full border-dashed border-white border">
-          <PlusCircle className="mr-2 w-4 h-4" />
-          New Chat
-        </Button>
-      </Link>
+      <Dialog>
+        <DialogTrigger asChild>
+          <Button className="w-full border-dashed border-white border">
+            <PlusCircle className="mr-2 w-4 h-4" />
+            New Chat
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Upload a new File</DialogTitle>
+          </DialogHeader>
+          <div>
+            <FileUpload />
+          </div>
+        </DialogContent>
+      </Dialog>
 
       <div className="flex pb-2 flex-col gap-2 mt-4 overflow-scroll">
         {chats.map((chat) => (
