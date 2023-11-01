d05d0037fcfabd7cf4b7f5e585bfe930a77bd3e6
Scott Zeta <wc070211@gmail.com>
2023-11-01 19:34:37 +1030

    recall googleviewer for every 3 seconds

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,11 +1,36 @@
-import React from 'react';
+'use client';
+import { useEffect, useRef, useState, useCallback } from 'react';
 
 type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
+  const [iframeTimeoutId, setIframeTimeoutId] = useState<any>();
+  const iframeRef: any = useRef(null);
+
+  const getIframeLink = useCallback(() => {
+    return `https://docs.google.com/gview?url=${pdf_url}&embedded=true`;
+  }, [pdf_url]);
+
+  const updateIframeSrc = useCallback(() => {
+    if (iframeRef.current) {
+      iframeRef!.current!.src = getIframeLink();
+    }
+  }, [getIframeLink]);
+
+  useEffect(() => {
+    const intervalId = setInterval(updateIframeSrc, 1000 * 3);
+    setIframeTimeoutId(intervalId);
+  }, [updateIframeSrc]);
+
+  function iframeLoaded() {
+    clearInterval(iframeTimeoutId);
+  }
   return (
     <iframe
-      src={`https://docs.google.com/gview?url=${pdf_url}&embedded=true`}
+      src={getIframeLink()}
+      ref={iframeRef}
+      onLoad={iframeLoaded}
+      onError={updateIframeSrc}
       className="w-full h-full"
     ></iframe>
   );
