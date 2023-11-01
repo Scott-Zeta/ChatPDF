dae04c083325cd85c7d7833fadbb4a69a70df63f
Scott Zeta <wc070211@gmail.com>
2023-11-01 20:08:48 +1030

    improve with feedback and backoff method

--- src/components/PDFViewer.tsx
+++ src/components/PDFViewer.tsx
@@ -1,15 +1,31 @@
 'use client';
+import { interval } from 'drizzle-orm/pg-core';
 import { useEffect, useRef, useState, useCallback } from 'react';
 
 type Props = { pdf_url: string };
 
 const PDFViewer = ({ pdf_url }: Props) => {
   const [iframeTimeoutId, setIframeTimeoutId] = useState<any>();
+  const [intervalTime, setIntervalTime] = useState<number>(1000 * 3); //start from 3 seconds
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isFail, setIsFail] = useState<boolean>(false);
   const iframeRef: any = useRef(null);
 
   const getIframeLink = useCallback(() => {
     return `https://docs.google.com/gview?url=${pdf_url}&embedded=true`;
   }, [pdf_url]);
+
+  const handleError = () => {
+    if (intervalTime < 10001) {
+      setIntervalTime((t) => t + 1000);
+    } else {
+      clearInterval(iframeTimeoutId);
+      setIsLoading(false);
+      setIsFail(true);
+      return;
+    }
+    updateIframeSrc();
+  };
 
   const updateIframeSrc = useCallback(() => {
     if (iframeRef.current) {
@@ -18,21 +34,34 @@ const PDFViewer = ({ pdf_url }: Props) => {
   }, [getIframeLink]);
 
   useEffect(() => {
-    const intervalId = setInterval(updateIframeSrc, 1000 * 3);
+    const intervalId = setInterval(updateIframeSrc, intervalTime);
     setIframeTimeoutId(intervalId);
-  }, [updateIframeSrc]);
+    return () => clearInterval(intervalId);
+  }, [updateIframeSrc, intervalTime]);
 
   function iframeLoaded() {
     clearInterval(iframeTimeoutId);
+    setIsLoading(false);
   }
   return (
-    <iframe
-      src={getIframeLink()}
-      ref={iframeRef}
-      onLoad={iframeLoaded}
-      onError={updateIframeSrc}
-      className="w-full h-full"
-    ></iframe>
+    <>
+      {isLoading && <p>Loading your document preview...</p>}
+      {!isFail && (
+        <iframe
+          src={getIframeLink()}
+          ref={iframeRef}
+          onLoad={iframeLoaded}
+          onError={handleError}
+          className="w-full h-full"
+        ></iframe>
+      )}
+      {isFail && (
+        <p>
+          We can not load your document preview, perhaps because the file is too
+          large or Google preview Service Issue, try again if you want.
+        </p>
+      )}
+    </>
   );
 };
 
